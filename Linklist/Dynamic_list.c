8c79ebb6cee24f4c514d3395565732d6a4131512
swaysigh <assentioncreed3@gmail.com>
2024-05-29 20:45:53 +0530

    added code

---
+++ Linklist/Dynamic_list.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int info;
+    struct node *link;
+};
+  void main(){
+    struct node *temp = malloc(sizeof(struct node));
+    struct node *temp1 = malloc(sizeof(struct node));
+    struct node *temp2 = malloc(sizeof(struct node));
+
+    temp -> info = 10;
+    temp1 -> info = 20;
+    temp2 -> info = 30;
+
+    temp -> link = temp1;
+    temp1 -> link = temp2;
+    temp2 -> link = NULL;
+
+    struct node *start = temp;
+  //traversing the list meaning accessing the list elements.
+struct node *ptr = start;
+while(ptr!=NULL){
+    printf("%d",ptr -> info);
+    ptr = ptr -> link;
+}
+    // Freeing the allocated memory
+    free(temp);
+    free(temp1);
+    free(temp2);
+
+
+}
